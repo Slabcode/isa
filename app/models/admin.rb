87c52efc8b61a67dd89c2411e7e8063363f604d6
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-08-13 13:40:41 -0500

    Validations We have added some basic validations to our models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,6 +5,13 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable, :timeoutable
 
+
+  validates_presence_of :email,:name,:lastname,:username,:mobile
+  validates_length_of :name,:lastname, minimum: 4, maximum: 30
+  validates_length_of :mobile, in: 10..12
+  validates_uniqueness_of :mobile,:username,:email
+  validates_numericality_of :mobile,:only_integer => true
+
   def self.load_admins(**args)
     all.paginate(page: args[:page] || 1, per_page: args[:per_page] || 10)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,14 @@ class User < ApplicationRecord
     "Revisor Tecnico" => 1,
   }
 
+  validates_presence_of :email,:name,:lastname,:mobile,:username
+  validates_length_of :name,:lastname, minimum: 4, maximum: 30
+  validates_length_of :mobile, in: 10..12
+  validates_uniqueness_of :mobile,:username,:email
+  validates_inclusion_of :role, :in => roles.keys
+  validates_numericality_of :mobile,:only_integer => true
+
+
   def self.load_users(**args)
     u = all.paginate(page: args[:page] || 1,per_page: args[:per_page] || 10)
     if args.has_key?(:role)
